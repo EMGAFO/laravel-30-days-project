05b6c584572a729f10bde42375322555dafde852
emgarcia <emgarcia@voiceteam.com>
2025-04-22 09:24:10 -0400

    some changes

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -5,11 +5,12 @@
 
     <ul>
         @foreach ($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline">
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
+                <div>
                     <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
-                </a>
-            </li>
+                </div>
+            </a>
         @endforeach
     </ul>
 </x-layout>
